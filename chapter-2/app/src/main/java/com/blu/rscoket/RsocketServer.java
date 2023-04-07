0ac7d5ae13ccb25d2422c876178c9424d30377d7
Shamim <srecon@yandex.ru>
2023-04-07 12:37:34 +0300

    RScoket request-response

---
+++ chapter-2/app/src/main/java/com/blu/rscoket/RsocketServer.java
@@ -0,0 +1,16 @@
+package com.blu.rscoket;
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@Slf4j
+public class RsocketServer {
+    public static void main(String[] args) {
+        log.info ("Welcome to Rsocket!!");
+        SpringApplication.run (RsocketServer.class, args);
+    }
+}
