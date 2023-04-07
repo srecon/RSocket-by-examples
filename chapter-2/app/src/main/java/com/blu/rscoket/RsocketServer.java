c26d4aaa23ee802237e75a6550247c56c9a3752e
Shamim <srecon@yandex.ru>
2023-04-07 16:39:58 +0300

    Add description

--- chapter-2/app/src/main/java/com/blu/rscoket/RsocketServer.java
+++ chapter-2/app/src/main/java/com/blu/rscoket/RsocketServer.java
@@ -3,14 +3,21 @@ package com.blu.rscoket;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @Slf4j
 public class RsocketServer {
+
+    @Value("${spring.rsocket.server.port}")
+    private int port;
+
     public static void main(String[] args) {
-        log.info ("Welcome to Rsocket!!");
+
         SpringApplication.run (RsocketServer.class, args);
+        log.info ("RSocket Server will be started on port: " + new RsocketServer().port);
     }
+
 }
