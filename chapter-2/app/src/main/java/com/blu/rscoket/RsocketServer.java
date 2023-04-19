d566ebaf2acff925b82767ee9f3e6277c828a744
Shamim <srecon@yandex.ru>
2023-04-19 18:38:21 +0300

    fix typos

--- chapter-2/app/src/main/java/com/blu/rscoket/RsocketServer.java
+++ chapter-2/app/src/main/java/com/blu/rscoket/RsocketServer.java
@@ -1,23 +1,27 @@
 package com.blu.rscoket;
 
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 @Slf4j
 public class RsocketServer {
 
-    @Value("${spring.rsocket.server.port}")
-    private int port;
+    //@Value("${spring.rsocket.server.port}")
+    //private int port;
+    //@Autowired
+    //private static AppConfiguration appConfiguration;
 
     public static void main(String[] args) {
 
         SpringApplication.run (RsocketServer.class, args);
-        log.info ("RSocket Server will be started on port: " + new RsocketServer().port);
+        //log.info ("RSocket Server will be started on port: " + appConfiguration.getPort());
     }
 
 }
