47bde293077f5e4108c02a2be0ca08334bb29613
Shamim <srecon@yandex.ru>
2023-04-25 14:09:18 +0300

    Restructred

--- chapter-2/Responder/app/src/main/java/com/blu/rscoket/RsocketServer.java
+++ chapter-2/Responder/app/src/main/java/com/blu/rscoket/RsocketServer.java
@@ -1,22 +1,16 @@
 package com.blu.rscoket;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.PropertySource;
+import org.springframework.boot.autoconfigure.rsocket.RSocketServerAutoConfiguration;
 
-import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 @Slf4j
+//@EnableAutoConfiguration(exclude={RSocketServerAutoConfiguration.class})
 public class RsocketServer {
-
-    //@Value("${spring.rsocket.server.port}")
-    //private int port;
-    //@Autowired
-    //private static AppConfiguration appConfiguration;
 
     public static void main(String[] args) {
 
